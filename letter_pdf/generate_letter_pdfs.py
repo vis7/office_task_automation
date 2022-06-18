587f0e62c0b709d18a9167c53e031ad8927488cf
vis7 <vishvajeetramanuj95@gmail.com>
2022-06-18 14:36:00 +0530

    made function of send_confirmation_letter

--- letter_pdf/generate_letter_pdfs.py
+++ letter_pdf/generate_letter_pdfs.py
@@ -17,60 +17,62 @@ from openpyxl import load_workbook
 from letter_pdf.utils import send_mail, create_session, destroy_session, convert_to_pdf
 
 os_type = platform.system()
-
-# getting name from excel sheet
-workbook = load_workbook('data/student list.xlsx')
-sheet = workbook.active
-sample_doc_dir = 'docs/'
-out_folder = 'pdfs/'
+sample_doc_dir = 'letter_pdf/docs/'
 
 session = create_session()
 
-# generate letter in doc format
-for i in range(1, 1000):
-    name = sheet.cell(row=i, column=2).value
-    subject = sheet.cell(row=i, column=3).value
-    date = sheet.cell(row=i, column=1).value
-    email = sheet.cell(row=i, column=4).value
+# create folders if not exist
 
-    # refine email
-    if email:
-        email = email.strip()
+def send_confirmation_letter(excel_file_path):
+    # getting name from excel sheet
+    workbook = load_workbook(excel_file_path)
+    sheet = workbook.active
 
-    # reformate date to proper format
-    if date: # if date or any field is missing then skip the row
-        # date = date.strftime('%d/%m/%Y')
+    # generate letter in doc format
+    for i in range(1, 1000):
+        name = sheet.cell(row=i, column=2).value
+        subject = sheet.cell(row=i, column=3).value
+        date = sheet.cell(row=i, column=1).value
+        email = sheet.cell(row=i, column=4).value
 
-        print(f"Generating certificate for: {name} {subject} {date}")
-
-        #######################################
-        # code for replacing name in doc
-        #######################################
-        #open the document
-        doc=Document('data/joining_letter_template.docx')
-
-        for p in doc.paragraphs:
-            inline = p.runs
-            for i in range(len(inline)):
-                text = inline[i].text
-                text=text.replace('Name',name)
-                text=text.replace('Subject', subject)
-                text=text.replace('Date', date)
-                inline[i].text = text
-        
-        # save changed document
-        letter_doc_path = f'./docs/Internship Confirmation {name}.docx'
-        doc.save(letter_doc_path)
-
-        convert_to_pdf(letter_doc_path, out_folder)
-
+        # refine email
         if email:
-            # sending mail
-            letter_pdf_path = f'./pdfs/Internship Confirmation {name}.pdf'
-            send_mail(email, session, letter_pdf_path)
+            email = email.strip()
 
-destroy_session(session)
+        # reformate date to proper format
+        if date: # if date or any field is missing then skip the row
+            # date = date.strftime('%d/%m/%Y')
 
-# remove docs
-for file_name in os.listdir(sample_doc_dir):
-    os.remove(os.path.join(sample_doc_dir, file_name))
+            print(f"Generating certificate for: {name} {subject} {date}")
+
+            #######################################
+            # code for replacing name in doc
+            #######################################
+            #open the document
+            doc=Document('letter_pdf/data/joining_letter_template.docx')
+
+            for p in doc.paragraphs:
+                inline = p.runs
+                for i in range(len(inline)):
+                    text = inline[i].text
+                    text=text.replace('Name',name)
+                    text=text.replace('Subject', subject)
+                    text=text.replace('Date', date)
+                    inline[i].text = text
+            
+            # save changed document
+            letter_doc_path = f'letter_pdf/docs/Internship Confirmation {name}.docx'
+            doc.save(letter_doc_path)
+
+            letter_pdf_path = f'letter_pdf/pdfs/Internship Confirmation {name}.pdf'
+            convert_to_pdf(letter_doc_path, letter_pdf_path)
+
+            if email:
+                # sending mail
+                send_mail(email, session, letter_pdf_path)
+
+    destroy_session(session)
+
+    # remove docs
+    for file_name in os.listdir(sample_doc_dir):
+        os.remove(os.path.join(sample_doc_dir, file_name))
---
+++ main.py
@@ -0,0 +1,4 @@
+from letter_pdf.generate_letter_pdfs import send_confirmation_letter
+
+if __name__ == "__main__":
+    send_confirmation_letter('letter_pdf/data/student list.xlsx')
