a5a0ab26cccc3f7214912476c6b737f128ca42ee
vis7 <vishvajeetramanuj95@gmail.com>
2022-06-18 17:19:09 +0530

    ready for exe

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+import tkinter
+from tkinter.filedialog import askopenfilename
 from letter_pdf.generate_letter_pdfs import send_confirmation_letter
 
+def submitFunction():
+    filename = askopenfilename(filetypes=(("Excel File", "*.xlsx"),))
+    print(f'filename is {filename}')
+    send_confirmation_letter(filename)
+    print("All letter sent successfully.")
+    print("Kindly close the application")
+
 if __name__ == "__main__":
-    send_confirmation_letter('letter_pdf/data/student list.xlsx')
+    main_window = tkinter.Tk(className='demo_window')
+    main_window.geometry("400x200")
+
+    button_submit = tkinter.Button(main_window, text="Select the File", command=submitFunction)
+    button_submit.config(width=20, height=2)
+
+    button_submit.pack()
+    main_window.mainloop()
